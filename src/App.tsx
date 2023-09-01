9d887e0c0389ebb8314f3618fd36a6efe8a4b1b2
Jungkyu Yang <didwjd8590412@gmail.com>
2023-09-01 18:11:40 +0900

    feat: LoadingSpinner 추가 및 동적 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 
 import { ROUTE_PATH } from './utils/constants/constants';
 
-import Main from './pages/MainPage';
-import IssueList from './pages/IssueListPage';
-import IssueDetail from './pages/IssueDetailPage';
-import NotFound from './pages/NotFoundPage';
-import Layout from './components/Layout/Layout';
+import LoadingSpinner from './components/common/LoadingSpinner';
+const Main = lazy(() => import('./pages/MainPage'));
+const IssueList = lazy(() => import('./pages/IssueListPage'));
+const IssueDetail = lazy(() => import('./pages/IssueDetailPage'));
+const NotFound = lazy(() => import('./pages/NotFoundPage'));
+const Layout = lazy(() => import('./components/Layout/Layout'));
 
 function App() {
 	return (
-		<Suspense fallback={<div>Loading</div>}>
+		<Suspense fallback={<LoadingSpinner />}>
 			<Routes>
 				<Route path={ROUTE_PATH.MAIN} element={<Main />} />
 				<Route element={<Layout />}>
--- src/components/IssueList/Issue.tsx
+++ src/components/IssueList/Issue.tsx
@@ -4,12 +4,12 @@ export default function Issue({ data }: any) {
 	return (
 		<div className={styles.ad_container}>
 			<div>
-				<span className={styles.ad_id}>#{data.number}</span>
+				<span className={styles.ad_id}>{`# ${data.number}`}</span>
 				<h2 className={styles.ad_title}>{data.title}</h2>
 			</div>
 			<div className={styles.ad_meta}>
-				<span className={styles.ad_user}>{data.user?.login}</span>
-				<span className={styles.ad_date}>{data.updated_at}</span>
+				<span className={styles.ad_user}>{`작성자: ${data.user?.login}`}</span>
+				<span className={styles.ad_date}>{`작성일: ${data.updated_at}`}</span>
 			</div>
 			<div>{data.comments}</div>
 		</div>
